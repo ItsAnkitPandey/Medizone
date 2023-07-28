9e92956663b5afbe7d3550f4cb4250c679bc12a4
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-07-28 21:23:06 +0530

    Heading updated

--- src/components/AllMedicines.jsx
+++ src/components/AllMedicines.jsx
@@ -1,42 +1,51 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { products } from './Products'
 
 
-const AllMedicines = ({addToCart}) => {
-   
+const AllMedicines = ({ addToCart }) => {
+
 
     return (
-        <div className='table-data'>
-            <table className="table" id="">
-                <thead>
-                    <tr>
-                        <th>Sr No.</th>
-                        <th>Image</th>
-                        <th>Medicine Name</th>
-                        <th>Price</th>
-                        <th>Action</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products.length === 0 ? (
+        <>
+        <div className="contact-hed">
+                <div className='med-heading'>
+                    <h1>Medicine List</h1>
+                    <div className='line'></div>
+                </div>
+            </div>
+            <div className='table-data'>
+                <table className="table" id="">
+                    <thead>
                         <tr>
-                            <td colSpan="4">Loading...</td>
+                            <th>Sr No.</th>
+                            <th>Image</th>
+                            <th>Medicine Name</th>
+                            <th>Price</th>
+                            <th>Action</th>
                         </tr>
-                    ) : (
-                        products.map((medicine) => (
-                            <tr key={medicine.id}>
-                                <td>{medicine.id}</td>
-                                <td><img src={medicine.img} alt="" /></td>
-                                <td>{medicine.name}</td>
-                                <td className='price'>{medicine.price}</td>
-                                <td><button onClick={()=>{addToCart(medicine)}}>Add to cart</button></td>
+                    </thead>
+                    <tbody>
+                        {products.length === 0 ? (
+                            <tr>
+                                <td colSpan="4">Loading...</td>
                             </tr>
-                        ))
-                    )}
+                        ) : (
+                            products.map((medicine) => (
+                                <tr key={medicine.id}>
+                                    <td>{medicine.id}</td>
+                                    <td><img src={medicine.img} alt="" /></td>
+                                    <td>{medicine.name}</td>
+                                    <td className='price'>{medicine.price}</td>
+                                    <td><button onClick={() => { addToCart(medicine) }}>Add to cart</button></td>
+                                </tr>
+                            ))
+                        )}
 
-                </tbody>
-            </table>
-        </div>
+                    </tbody>
+                </table>
+            </div>
+        </>
+
     );
 };
 
