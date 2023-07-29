d40e6e5d73e72d0ada033b9e3b8d14c289803d07
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-07-29 11:28:10 +0530

    Making footer and carousel responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,28 +6,30 @@ const Footer = () => {
 	return (
 		<div className="footer-wrapper">
 			<footer>
-				<div className="col-50">
-					<div className="logo ">
-						<img src={logo} alt="" />
+				<div className="footer">
+					<div className="col-50">
+						<div className="logo2 ">
+							<img src={logo} alt="" />
+						</div>
+						<p className="sub-text">Your one-stop destination for all your medicinal needs,<br /> offering a wide range of high-quality products<br /> and convenient online shopping experience</p>
 					</div>
-					<p className="sub-text">Your one-stop destination for all your medicinal needs,<br /> offering a wide range of high-quality products<br /> and convenient online shopping experience</p>
-				</div>
-				<div className="col-25">
-					<h3>Quick Links</h3>
-					<ul>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to="">Blog</Link></li>
-						<li><Link to="/contact">Contact Us</Link></li>
-					</ul>
+					<div className="col-25">
+						<h3>Quick Links</h3>
+						<ul>
+							<li><Link to='/'>Home</Link></li>
+							<li><Link to="">Blog</Link></li>
+							<li><Link to="/contact">Contact Us</Link></li>
+						</ul>
 
-				</div>
-				<div className="col-25">
-					<h3>About</h3>
-					<ul>
-						<li><Link to="">Shiping</Link></li>
-						<li><Link to="">Terms & Conditions</Link></li>
-						<li><Link to="">Privacy Policy</Link></li>
-					</ul>
+					</div>
+					<div className="col-26">
+						<h3>About</h3>
+						<ul>
+							<li><Link to="">Shiping</Link></li>
+							<li><Link to="">Terms & Conditions</Link></li>
+							<li><Link to="">Privacy Policy</Link></li>
+						</ul>
+					</div>
 				</div>
 				<hr />
 				<div className="footer-end">
