df245855f97a7490661233a8d4feee1d62513a20
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-07-31 17:24:31 +0530

    added redirect for home

--- src/components/Thankyou.jsx
+++ src/components/Thankyou.jsx
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom'
 import './thankyou.css'
 const Thankyou = () => {
-    // const [count, setCount] = useState(7);
-    // const redirect = "https://localhost:3000"; // The redirection's landing page.
+    const [count, setCount] = useState(7);
+    const redirect = "https://medizon.netlify.app"; // The redirection's landing page.
 
-    // useEffect(() => {
-    //   const interval = setInterval(() => {
-    //     setCount((prevCount) => {
-    //       if (prevCount === 0) {
-    //         window.location.href = redirect;
-    //       }
-    //       return prevCount - 1;
-    //     });
-    //   }, 1000);
+    useEffect(() => {
+      const interval = setInterval(() => {
+        setCount((prevCount) => {
+          if (prevCount === 0) {
+            window.location.href = redirect;
+          }
+          return prevCount - 1;
+        });
+      }, 1000);
 
-    //   return () => {
-    //     clearInterval(interval);
-    //   };
-    // }, [redirect]);
+      return () => {
+        clearInterval(interval);
+      };
+    }, [redirect]);
     return (
         <div className='thank-container'>
             <div className="thank">
