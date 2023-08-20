1d9f86f1fcdd6f940febf2e7d26e8a2c977b2b14
Ankit Pandey <ankitpandey.272003@gmail.com>
2023-08-20 16:26:16 +0530

    removed unusual lines

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import { Link,useNavigate } from 'react-router-dom';
-import { Url } from '../utils/Url';
 import axios from 'axios';
 
 const Signup = () => {
