deccf0dcfa8f42f801d46f278ce3124b496a7605
Ankit Pandey <ankitpandey.272003@gmail.com>
2026-01-18 21:31:47 +0530

    refactor: reorganize cart components
    - Move CartItem to components/cart/
    - Move TotalPrice to components/cart/

---
+++ src/components/cart/CartItem.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const CartItem = ({ name, img, quantity, price, id, cart, setCart }) => {
+  const handleRemove = () => {
+    const updatedCart = cart.filter((item) => item.id !== id);
+    setCart(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+  };
+
+  const handleIncrease = () => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id) {
+        return { ...item, quantity: item.quantity + 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+  };
+
+  const handleDecrease = () => {
+    const updatedCart = cart.map((item) => {
+      if (item.id === id && item.quantity > 1) {
+        return { ...item, quantity: item.quantity - 1 };
+      }
+      return item;
+    });
+    setCart(updatedCart);
+    localStorage.setItem('cart', JSON.stringify(updatedCart));
+  };
+  return (
+    <div className="cart-item-row">
+      <div className="product">
+        <i className="fa-regular fa-trash-can" onClick={handleRemove}></i>
+        <img className="cart-img" src={img} alt="medicine" />
+        <span className='product_name'>{name}</span>
+      </div>
+      <div className="price-cart">&#8377;{price}</div>
+      <div className="quantity">
+        <i className="fa-solid fa-minus" onClick={handleDecrease}></i> 
+        <span>{quantity}</span>
+        <i className="fa-solid fa-plus" onClick={handleIncrease} ></i> 
+      </div>
+      <div className="total">
+        &#8377;{(quantity * price).toFixed(2)}
+      </div>
+    </div>
+  )
+}
+
+export default CartItem
